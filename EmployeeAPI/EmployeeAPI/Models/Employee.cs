e165e2f71e2ec8343f1113a60f2e105e65bb80d6
Sasika Dilshan <sasika2000dilshan@gmail.com>
2025-09-28 21:00:55 +0530

    Backend

---
+++ EmployeeAPI/EmployeeAPI/Models/Employee.cs
@@ -0,0 +1,12 @@
+﻿namespace EmployeeAPI.Models
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }  // Primary Key
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public decimal Salary { get; set; }
+        public int DepartmentId { get; set; } // Foreign Key
+    }
+}
